50fca6c237eaf440988718da05ccc459da7c485e
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-04-23 14:39:06 -0300

    docs - Adicionado template de pull request do github

--- src/main/java/br/edu/utfpr/pb/ext/server/error/ErrorHandler.java
+++ src/main/java/br/edu/utfpr/pb/ext/server/error/ErrorHandler.java
@@ -12,30 +12,30 @@ import org.springframework.web.context.request.WebRequest;
 @RestController
 public class ErrorHandler implements ErrorController {
 
-    private final ErrorAttributes errorAttributes;
+  private final ErrorAttributes errorAttributes;
 
-    public ErrorHandler(ErrorAttributes errorAttributes) {
-        this.errorAttributes = errorAttributes;
-    }
+  public ErrorHandler(ErrorAttributes errorAttributes) {
+    this.errorAttributes = errorAttributes;
+  }
 
-    @GetMapping("/error")
-    public ApiError handleError(WebRequest webRequest) {
-        return buildApiError(webRequest);
-    }
+  @GetMapping("/error")
+  public ApiError handleError(WebRequest webRequest) {
+    return buildApiError(webRequest);
+  }
 
-    @PostMapping("/error")
-    public ApiError handleUnsafeError(WebRequest webRequest) {
-        return buildApiError(webRequest);
-    }
+  @PostMapping("/error")
+  public ApiError handleUnsafeError(WebRequest webRequest) {
+    return buildApiError(webRequest);
+  }
 
-    private ApiError buildApiError(WebRequest webRequest) {
-        Map<String, Object> attributes = errorAttributes.getErrorAttributes(
-                webRequest,
-                ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE));
-        return ApiError.builder()
-                .message((String) attributes.get("message"))
-                .url((String) attributes.get("path"))
-                .status((Integer) attributes.get("status"))
-                .build();
-    }
+  private ApiError buildApiError(WebRequest webRequest) {
+    Map<String, Object> attributes =
+        errorAttributes.getErrorAttributes(
+            webRequest, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE));
+    return ApiError.builder()
+        .message((String) attributes.get("message"))
+        .url((String) attributes.get("path"))
+        .status((Integer) attributes.get("status"))
+        .build();
+  }
 }
--- src/main/java/br/edu/utfpr/pb/ext/server/error/ExceptionHandlerAdvice.java
+++ src/main/java/br/edu/utfpr/pb/ext/server/error/ExceptionHandlerAdvice.java
@@ -1,6 +1,8 @@
 package br.edu.utfpr.pb.ext.server.error;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,27 +10,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception,
-            HttpServletRequest request) {
-        Map<String, String> validationErrors = new HashMap<>();
-        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-
-        return ApiError.builder()
-                .status(400)
-                .message("Validation Error")
-                .url(request.getServletPath())
-                .validationErrors(validationErrors)
-                .build();
+  @ExceptionHandler({MethodArgumentNotValidException.class})
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ApiError handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException exception, HttpServletRequest request) {
+    Map<String, String> validationErrors = new HashMap<>();
+    for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+      validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
     }
+
+    return ApiError.builder()
+        .status(400)
+        .message("Validation Error")
+        .url(request.getServletPath())
+        .validationErrors(validationErrors)
+        .build();
+  }
 }
