bfa486b1b59162a6069f1482f38b55e074ae6684
Rodrigo <rodizipa2007@gmail.com>
2025-05-05 19:18:06 -0300

    chore - organização

--- src/main/java/br/edu/utfpr/pb/ext/server/inscricao/Inscricao.java
+++ src/main/java/br/edu/utfpr/pb/ext/server/inscricao/Inscricao.java
@@ -1,16 +1,15 @@
 package br.edu.utfpr.pb.ext.server.inscricao;
 
+import br.edu.utfpr.pb.ext.server.generics.BaseEntity;
 import br.edu.utfpr.pb.ext.server.projeto.Projeto;
 import br.edu.utfpr.pb.ext.server.usuario.Usuario;
-import br.edu.utfpr.pb.ext.server.generics.BaseEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import java.util.Date;
 import lombok.*;
 
-import java.util.Date;
-
 @Entity
-@Table(name="tb_inscricao")
+@Table(name = "tb_inscricao")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -18,16 +17,13 @@ import java.util.Date;
 @Builder
 public class Inscricao extends BaseEntity {
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "usuario_id")
-    @NotNull
-    private Usuario usuario;
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "usuario_id")
+  @NotNull private Usuario usuario;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "projeto_id")
-    @NotNull
-    private Projeto projeto;
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "projeto_id")
+  @NotNull private Projeto projeto;
 
-    @NotNull
-    private Date dataDeInscricao;
+  @NotNull private Date dataDeInscricao;
 }
--- src/main/java/br/edu/utfpr/pb/ext/server/sugestaoDeProjeto/SugestaoDeProjeto.java
+++ src/main/java/br/edu/utfpr/pb/ext/server/sugestaoDeProjeto/SugestaoDeProjeto.java
@@ -1,14 +1,14 @@
 package br.edu.utfpr.pb.ext.server.sugestaoDeProjeto;
 
 import br.edu.utfpr.pb.ext.server.curso.Curso;
-import br.edu.utfpr.pb.ext.server.usuario.Usuario;
 import br.edu.utfpr.pb.ext.server.generics.BaseEntity;
+import br.edu.utfpr.pb.ext.server.usuario.Usuario;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
-@Table(name="tb_sugestao_de_projeto")
+@Table(name = "tb_sugestao_de_projeto")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -16,21 +16,17 @@ import lombok.*;
 @Builder
 public class SugestaoDeProjeto extends BaseEntity {
 
-    @ManyToOne
-    @JoinColumn(name = "aluno_id")
-    @NotNull
-    private Usuario aluno;
+  @ManyToOne
+  @JoinColumn(name = "aluno_id")
+  @NotNull private Usuario aluno;
 
-    @ManyToOne
-    @JoinColumn(name = "professor_id")
-    @NotNull
-    private Usuario professor;
+  @ManyToOne
+  @JoinColumn(name = "professor_id")
+  @NotNull private Usuario professor;
 
-    @ManyToOne
-    @JoinColumn(name = "curso_id")
-    @NotNull
-    private Curso curso;
+  @ManyToOne
+  @JoinColumn(name = "curso_id")
+  @NotNull private Curso curso;
 
-    @NotNull
-    private String descricao;
+  @NotNull private String descricao;
 }
