e1e8287a4ed4c0668c867d80b49bcbb281891245
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-05-06 23:29:57 -0300

    chore - reorganização de pastas por funcionalidade

--- src/main/java/br/edu/utfpr/pb/ext/server/auth/AuthController.java
+++ src/main/java/br/edu/utfpr/pb/ext/server/auth/AuthController.java
@@ -1,4 +1,4 @@
-package br.edu.utfpr.pb.ext.server.controller;
+package br.edu.utfpr.pb.ext.server.auth;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
