748ac0f1815c7a6ab9ef9a325bd002880aa92053
geovanedcs <57137423+geovanedcs@users.noreply.github.com>
2025-05-11 18:09:25 -0300

    PEXT-39 feat: update Departamento enum and validation messages for user management

--- src/main/resources/db/migration/V1.1.2__update_tb_usuario.sql
+++ src/main/resources/db/migration/V1.1.2__update_tb_usuario.sql
@@ -3,3 +3,7 @@ ALTER TABLE tb_usuario RENAME COLUMN registro TO cpf;
 ALTER TABLE tb_usuario ADD COLUMN siape VARCHAR(7);
 ALTER TABLE tb_usuario ADD COLUMN registro_academico VARCHAR(9);
 ALTER TABLE tb_usuario ADD COLUMN departamento VARCHAR(50);
+
+ALTER TABLE tb_usuario ADD CONSTRAINT uq_usuario_cpf UNIQUE (cpf);
+ALTER TABLE tb_usuario ADD CONSTRAINT uq_usuario_siape UNIQUE (siape);
+ALTER TABLE tb_usuario ADD CONSTRAINT uq_usuario_registro_academico UNIQUE (registro_academico);
