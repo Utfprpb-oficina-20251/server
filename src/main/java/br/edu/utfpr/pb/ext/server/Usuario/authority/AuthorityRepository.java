74536611315522b07d791d744bf7bc71ea459638
Geovane Soares <57137423+geovanedcs@users.noreply.github.com>
2025-05-19 14:07:15 -0300

    PEXT-72 PEXT-62 PEXT-61 PEXT- 31 feat: implement Authority entity and validation for unique RA in Usuario
    Signed-off-by: Geovane Soares <57137423+geovanedcs@users.noreply.github.com>

--- src/main/java/br/edu/utfpr/pb/ext/server/Usuario/authority/AuthorityRepository.java
+++ src/main/java/br/edu/utfpr/pb/ext/server/Usuario/authority/AuthorityRepository.java
@@ -1,9 +1,10 @@
 package br.edu.utfpr.pb.ext.server.usuario.authority;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-    Authority findByAuthority(String authority);
+  Optional<Authority> findByAuthority(String authority);
 }
