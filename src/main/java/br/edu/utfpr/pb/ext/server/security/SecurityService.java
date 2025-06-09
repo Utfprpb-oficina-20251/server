bb79a19204362fd51b46f8125ff56094592900e6
thiagok1 <thiagok1.2710@gmail.com>
2025-06-08 22:51:40 -0300

    revisão de variaveis e imports não utilizados

--- src/main/java/br/edu/utfpr/pb/ext/server/security/SecurityService.java
+++ src/main/java/br/edu/utfpr/pb/ext/server/security/SecurityService.java
@@ -4,20 +4,16 @@ import br.edu.utfpr.pb.ext.server.projeto.Projeto;
 import br.edu.utfpr.pb.ext.server.projeto.ProjetoRepository;
 import br.edu.utfpr.pb.ext.server.usuario.Usuario;
 import br.edu.utfpr.pb.ext.server.usuario.UsuarioRepository;
-import br.edu.utfpr.pb.ext.server.usuario.UsuarioServiceImpl;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-@Service("securityService") // O nome do bean que a SpEL procura
+@Service("securityService")
 public class SecurityService {
 
     private final ProjetoRepository projetoRepository;
-    private final UsuarioRepository usuarioRepository;
 
-    // Injeção de dependências necessárias
-    public SecurityService(ProjetoRepository projetoRepository, UsuarioRepository usuarioRepository) {
+    public SecurityService(ProjetoRepository projetoRepository) {
         this.projetoRepository = projetoRepository;
-        this.usuarioRepository = usuarioRepository;
     }
 
     public boolean podeEditarProjeto(Long projetoId) {
@@ -27,9 +23,7 @@ public class SecurityService {
         Projeto projeto = projetoRepository.findById(projetoId)
                 .orElseThrow(() -> new RuntimeException("Projeto não encontrado para verificação de segurança"));
 
-        boolean isDaEquipe = projeto.getEquipeExecutora().stream()
+        return  projeto.getEquipeExecutora().stream()
                 .anyMatch(membro -> membro.getId().equals(usuarioLogadoId));
-
-        return  isDaEquipe;
     }
 }
