f099523207d70ea8bd1d0b9ce0af1abc831c3321
thiagok1 <thiagok1.2710@gmail.com>
2025-06-16 22:08:27 -0300

    correção em testes unitarios

--- src/test/java/br/edu/utfpr/pb/ext/server/projeto/ProjetoControllerTest.java
+++ src/test/java/br/edu/utfpr/pb/ext/server/projeto/ProjetoControllerTest.java
@@ -201,10 +201,9 @@ class ProjetoControllerTest {
 
     FiltroProjetoDTO filtroEspecifico =
         new FiltroProjetoDTO(null, null, null, null, 2L, null, null);
--   when(projetoService.buscarProjetosPorFiltro(eq(filtroEspecifico)))
--       .thenReturn(Collections.emptyList());
-+   when(projetoService.buscarProjetosPorFiltro(any(FiltroProjetoDTO.class)))
-+       .thenReturn(Collections.emptyList());
+
+   when(projetoService.buscarProjetosPorFiltro(any(FiltroProjetoDTO.class)))
+       .thenReturn(Collections.emptyList());
 
     // Act (Ação) 🚀
     ResponseEntity<List<ProjetoDTO>> response = projetoController.buscarMeusProjetos(usuarioLogado);
@@ -215,9 +214,7 @@ class ProjetoControllerTest {
 
     assertNotNull(response.getBody());
     assertTrue(response.getBody().isEmpty());
-
--   verify(projetoService, times(1)).buscarProjetosPorFiltro(eq(filtroEspecifico));
-+   verify(projetoService, times(2)).buscarProjetosPorFiltro(any(FiltroProjetoDTO.class));
+    verify(projetoService, times(1)).buscarProjetosPorFiltro(any(FiltroProjetoDTO.class));
   }
   @Test
   void buscarProjetos_semFiltros_retornaOkComListaDeProjetos() {
