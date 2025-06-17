25bd6f1c25607ff3a9d4cd477809b088fd1fa438
geovanedcs <57137423+geovanedcs@users.noreply.github.com>
2025-06-16 22:32:05 -0300

    PEXT-151 feat: add cargaHoraria and qtdeVagas fields to Projeto and ProjetoDTO, and update related services

--- src/main/java/br/edu/utfpr/pb/ext/server/projeto/Projeto.java
+++ src/main/java/br/edu/utfpr/pb/ext/server/projeto/Projeto.java
@@ -5,8 +5,10 @@ import br.edu.utfpr.pb.ext.server.projeto.enums.StatusProjeto;
 import br.edu.utfpr.pb.ext.server.usuario.Usuario;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+
 import java.util.Date;
 import java.util.List;
+
 import lombok.*;
 
 @Entity
@@ -18,36 +20,46 @@ import lombok.*;
 @Builder
 public class Projeto extends BaseEntity {
 
-  @NotNull private String titulo;
+    @NotNull
+    private String titulo;
 
-  @NotNull private String descricao;
+    @NotNull
+    private String descricao;
 
-  @NotNull private String justificativa;
+    @NotNull
+    private String justificativa;
 
-  @NotNull private Date dataInicio;
+    @NotNull
+    private Date dataInicio;
 
-  private Date dataFim;
+    private Date dataFim;
 
-  @NotNull private String publicoAlvo;
+    @NotNull
+    private String publicoAlvo;
 
-  @NotNull private boolean vinculadoDisciplina;
+    @NotNull
+    private boolean vinculadoDisciplina;
 
-  private String restricaoPublico;
+    private String restricaoPublico;
 
-  @ManyToOne
-  @JoinColumn(name = "responsavel_id")
-  private Usuario responsavel;
+    @ManyToOne
+    @JoinColumn(name = "responsavel_id")
+    private Usuario responsavel;
 
-  @ManyToMany
-  @JoinTable(
-      name = "tb_equipe_servidor",
-      joinColumns = @JoinColumn(name = "id_projeto"),
-      inverseJoinColumns = @JoinColumn(name = "id_usuario"))
-  private List<Usuario> equipeExecutora;
+    @ManyToMany
+    @JoinTable(
+            name = "tb_equipe_servidor",
+            joinColumns = @JoinColumn(name = "id_projeto"),
+            inverseJoinColumns = @JoinColumn(name = "id_usuario"))
+    private List<Usuario> equipeExecutora;
 
-  @NotNull private StatusProjeto status;
+    @NotNull
+    private StatusProjeto status;
 
-  private Long cargaHoraria;
 
-  private Long qtdeVagas;
+    @Column(name = "carga_horaria")
+    private Long cargaHoraria;
+
+    @Column(name = "qtde_vagas")
+    private Long qtdeVagas;
 }
