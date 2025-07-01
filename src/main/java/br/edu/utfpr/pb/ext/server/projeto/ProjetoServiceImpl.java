9a1a34801b6619d39d4f24e5780e386ef8896987
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-06-30 21:25:33 -0300

    Revert "fix - responsável não atribuído"

--- src/main/java/br/edu/utfpr/pb/ext/server/projeto/ProjetoServiceImpl.java
+++ src/main/java/br/edu/utfpr/pb/ext/server/projeto/ProjetoServiceImpl.java
@@ -267,7 +267,6 @@ public class ProjetoServiceImpl extends CrudServiceImpl<Projeto, Long> implement
                 () -> new EntityNotFoundException("Projeto com ID " + id + " não encontrado."));
     modelMapper.map(dto, projeto);
     processaImagemUrl(projeto);
-    atribuirResponsavel(projeto);
     Projeto projetoAtualizado = getRepository().save(projeto);
 
     return modelMapper.map(projetoAtualizado, ProjetoDTO.class);
